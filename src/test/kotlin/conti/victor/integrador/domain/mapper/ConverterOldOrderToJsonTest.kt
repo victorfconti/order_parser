439fcca9a741c66816440a3048fb7b809c20b4a2
Victor Fernando Conti <victorfconti@gmail.com>
2025-05-18 21:43:06 -0300

    Criação dos testes para multiplas ordens

--- src/test/kotlin/conti/victor/integrador/domain/mapper/ConverterOldOrderToJsonTest.kt
+++ src/test/kotlin/conti/victor/integrador/domain/mapper/ConverterOldOrderToJsonTest.kt
@@ -27,7 +27,7 @@ class ConverterOldOrderToJsonTest {
             ],
         delimiter = ';'
     )
-    fun testaConverterSingleLine(received: String, expected: String) {
+    fun testConverterSingleLine(received: String, expected: String) {
         val response = ConverterOldOrderToJson(received).parser()
 
         val jsonCreated = objectMapper.writeValueAsString(response)
@@ -37,7 +37,7 @@ class ConverterOldOrderToJsonTest {
 
     @Test
     @DisplayName("Teste com data inválida")
-    fun testaConverterSingleLineInvalidData() {
+    fun testConverterSingleLineInvalidData() {
         val invalidDateLine = "0000000002                                     Medeiros00000123450000000111      256.2420201a01"
 
         assertThatThrownBy { ConverterOldOrderToJson(invalidDateLine).parser() }.isInstanceOf(IllegalArgumentException::class.java)
@@ -45,7 +45,7 @@ class ConverterOldOrderToJsonTest {
 
     @Test
     @DisplayName("Teste com linha de tamanho inválida")
-    fun testaConverterSingleLineInvalidSize() {
+    fun testConverterSingleLineInvalidSize() {
         val invalidDateLine = "0000000002                                     Medeiros00000123450000000111      256.242020101"
 
         assertThatThrownBy { ConverterOldOrderToJson(invalidDateLine).parser() }.isInstanceOf(IllegalArgumentException::class.java)
@@ -53,7 +53,7 @@ class ConverterOldOrderToJsonTest {
 
     @Test
     @DisplayName("Teste de conversão de duas linhas simples")
-    fun testaConverterTwoLines() {
+    fun testConverterTwoLines() {
         val jsonEsperado = "[{\"user_id\":1,\"name\":\"Zarelli\",\"orders\":[{\"order_id\":123,\"total\":\"512.24\",\"date\":\"2021-12-01\",\"products\":[{\"product_id\":122,\"value\":\"512.24\"}]}]},{\"user_id\":2,\"name\":\"Medeiros\",\"orders\":[{\"order_id\":12345,\"total\":\"256.24\",\"date\":\"2020-12-01\",\"products\":[{\"product_id\":111,\"value\":\"256.24\"}]}]}]"
 
         val fileContent = object {}.javaClass.getResourceAsStream("/data_two_lines.txt")
@@ -70,10 +70,27 @@ class ConverterOldOrderToJsonTest {
 
     @Test
     @DisplayName("Teste de conversão de 4 linhas, cada uma com mais de um produto")
-    fun testaConverterFourLinesMultiplesProducts() {
+    fun testConverterFourLinesMultiplesProducts() {
         val jsonEsperado = "[{\"user_id\":2,\"name\":\"Medeiros\",\"orders\":[{\"order_id\":12345,\"total\":\"512.48\",\"date\":\"2020-12-01\",\"products\":[{\"product_id\":111,\"value\":\"256.24\"},{\"product_id\":122,\"value\":\"256.24\"}]}]},{\"user_id\":1,\"name\":\"Zarelli\",\"orders\":[{\"order_id\":123,\"total\":\"1024.48\",\"date\":\"2021-12-01\",\"products\":[{\"product_id\":111,\"value\":\"512.24\"},{\"product_id\":122,\"value\":\"512.24\"}]}]}]"
 
         val fileContent = object {}.javaClass.getResourceAsStream("/data_four_lines_multiples_products.txt")
+            ?.bufferedReader()
+            ?.use { it.readText() }
+            ?: throw IllegalArgumentException("File not found")
+
+        val response = ConverterOldOrderToJson(fileContent).parser()
+        val jsonCreated = objectMapper.writeValueAsString(response)
+
+        assertThat(jsonCreated).isEqualTo(jsonEsperado)
+
+    }
+
+    @Test
+    @DisplayName("Teste de conversão de 4 linhas, cada uma com mais de uma ordem")
+    fun testConverterFourLinesMultiplesOrdens() {
+        val jsonEsperado = "[{\"user_id\":1,\"name\":\"Medeiros\",\"orders\":[{\"order_id\":12345,\"total\":\"512.48\",\"date\":\"2020-12-01\",\"products\":[{\"product_id\":111,\"value\":\"256.24\"},{\"product_id\":122,\"value\":\"256.24\"}]},{\"order_id\":123,\"total\":\"1024.48\",\"date\":\"2021-12-01\",\"products\":[{\"product_id\":111,\"value\":\"512.24\"},{\"product_id\":122,\"value\":\"512.24\"}]}]}]"
+
+        val fileContent = object {}.javaClass.getResourceAsStream("/data_four_lines_multiples_orders_and_products.txt")
             ?.bufferedReader()
             ?.use { it.readText() }
             ?: throw IllegalArgumentException("File not found")
